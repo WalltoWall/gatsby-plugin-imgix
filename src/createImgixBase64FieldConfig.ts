42f994648718272ed7c51cde63a8c5ebc67c6abe
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-04-19 17:48:29 -1000

    feat: allow custom URL resolver for base64 field

--- src/createImgixBase64FieldConfig.ts
+++ src/createImgixBase64FieldConfig.ts
@@ -4,23 +4,27 @@ import {
   GraphQLString,
   GraphQLFieldConfig,
 } from 'gatsby/graphql'
+import { FixedObject, FluidObject } from 'gatsby-image'
 
-import { fetchImgixBase64Url } from './shared'
+import { fetchImgixBase64Url, ImgixResolveUrl } from './shared'
 
 interface CreateImgixBase64UrlFieldConfigArgs {
-  cache: Cache['cache']
+  resolveUrl?: ImgixResolveUrl<FixedObject | FluidObject>
   secureUrlToken?: string
+  cache: Cache['cache']
 }
 
 export const createImgixBase64UrlFieldConfig = <TContext>({
-  cache,
+  resolveUrl = obj => obj.base64,
   secureUrlToken,
+  cache,
 }: CreateImgixBase64UrlFieldConfigArgs): GraphQLFieldConfig<
-  string,
+  FixedObject | FluidObject,
   TContext
 > => ({
   type: new GraphQLNonNull(GraphQLString),
-  resolve: async url => {
+  resolve: async obj => {
+    const url = await resolveUrl(obj)
     if (!url) return
 
     return await fetchImgixBase64Url({ url, cache, secureUrlToken })
